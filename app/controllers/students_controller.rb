3770f2ffbc569a915c62b386728801574d84ff7e
Your Name <samanbatool08@gmail.com>
2019-12-16 14:16:56 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,29 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+    render :show
+  end
+
+  def new
+    @student = Student.new
+    render :new
+  end
+
+  def create 
+    @student = Student.create(student_params)
+
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+
+    
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:last_name, :first_name)
   end
 
 end
